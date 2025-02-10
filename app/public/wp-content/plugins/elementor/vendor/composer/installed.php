3c47fb7a4d6122f5367e545ec6e0e28a8d17013e
3rdThomas <vado.thomas.pro@gmail.com>
2025-02-10 10:32:28 -0400

    Last push

--- app/public/wp-content/plugins/elementor/vendor/composer/installed.php
+++ app/public/wp-content/plugins/elementor/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'elementor/elementor',
         'pretty_version' => '3.27.x-dev',
         'version' => '3.27.9999999.9999999-dev',
-        'reference' => '34cb03fff03c524b411dc0422c0bfc4126f2e40b',
+        'reference' => '03d750de25fd5881951c1279c70992d71788f3f0',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'elementor/elementor' => array(
             'pretty_version' => '3.27.x-dev',
             'version' => '3.27.9999999.9999999-dev',
-            'reference' => '34cb03fff03c524b411dc0422c0bfc4126f2e40b',
+            'reference' => '03d750de25fd5881951c1279c70992d71788f3f0',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
